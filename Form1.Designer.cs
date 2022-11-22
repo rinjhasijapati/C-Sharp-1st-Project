bf06340414c72f6327e6e2778391ad07e6a2162e
rinjhasijapati <aastha_sijapati@yahoo.com>
2022-11-22 09:50:35 +0545

    new

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.username = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Submit = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // username
+            // 
+            this.username.AutoSize = true;
+            this.username.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.username.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.username.Location = new System.Drawing.Point(84, 36);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(35, 13);
+            this.username.TabIndex = 0;
+            this.username.Text = "Name";
+            this.username.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(125, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Submit
+            // 
+            this.Submit.Location = new System.Drawing.Point(140, 55);
+            this.Submit.Name = "Submit";
+            this.Submit.Size = new System.Drawing.Size(75, 23);
+            this.Submit.TabIndex = 2;
+            this.Submit.Text = "Submit";
+            this.Submit.UseVisualStyleBackColor = true;
+            this.Submit.Click += new System.EventHandler(this.Submit_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(280, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 122);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Submit);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.username);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label username;
+        private TextBox textBox1;
+        private Button Submit;
+        private PictureBox pictureBox1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,41 @@ namespace Third_Sem_Projet
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Submit_Click(object sender, EventArgs e)
+        {
+            string data = textBox1.Text;
+            if (data == "test")
+            {
+                MessageBox.Show("Success");
+            }
+            else
+            {
+                MessageBox.Show("Failed");
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                 MessageBox.Show("Display Time:  " + i);
+            }
+            // MessageBox.Show("Your name is " + data);
+
+         //   try
+         //   {
+        //        int age_input = Convert.ToInt32(age.text);
+         //       MessageBox.Show("Age: " + age_input);
+        //    }
+         //   catch (Exception ex)
+        //   {
+         //       MessageBox.Show("Invalid Number");
+
+
+         //   }
+
+        }
     }
 }
