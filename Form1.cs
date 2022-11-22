3eadea02b09cdf7a6a70fcf9fabc378190a8eb77
rinjhasijapati <aastha_sijapati@yahoo.com>
2022-11-22 08:37:44 +0545

    first commit

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace Third_Sem_Projet
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+namespace Third_Sem_Projet
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
